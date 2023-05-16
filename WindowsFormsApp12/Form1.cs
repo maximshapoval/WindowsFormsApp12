940cf77a9e8feb9fcc7911e3e3b1f95030a2aad6
maria <maria@WIN-L58HK5EKJ6M>
2023-05-16 18:06:54 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    using System;
+    using System.Windows.Forms;
+
+    
+        public partial class Form1 : Form
+        {
+            private Timer timer;
+            private int secondsPassed = 0;
+
+            public Form1()
+            {
+                InitializeComponent();
+
+                timer = new Timer();
+                timer.Interval = 1000;
+                timer.Tick += new EventHandler(Timer_Tick);
+            }
+
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                secondsPassed++;
+                string message = "Прошло " + secondsPassed + " сек";
+                listBox1.Items.Add(message);
+            }
+        }
+}
